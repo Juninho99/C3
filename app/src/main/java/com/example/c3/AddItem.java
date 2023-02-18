ee237f51eef227984ed5e276a478ec2d2406466c
Juninho99 <azunic1@etf.unsa.ba>
2023-02-18 21:00:13 +0100

    Dodat prozor za dodavanje novog artikla

---
+++ app/src/main/java/com/example/c3/AddItem.java
@@ -0,0 +1,14 @@
+package com.example.c3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item);
+    }
+}
--- app/src/main/java/com/example/c3/SelectedList.java
+++ app/src/main/java/com/example/c3/SelectedList.java
@@ -1,6 +1,16 @@
 package com.example.c3;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 public class SelectedList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selected_list);
+    }
+    public void dodajItem(View view) {
+    }
 }
