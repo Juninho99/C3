7225d5bbc4a91eb563cf9e419436f4092a7c2627
Juninho99 <azunic1@etf.unsa.ba>
2023-02-20 15:53:39 +0100

    Trebalo bi da unos liste radi dobro, slijedi testiranje

--- app/src/main/java/com/example/c3/AddList.java
+++ app/src/main/java/com/example/c3/AddList.java
@@ -2,13 +2,51 @@ package com.example.c3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddList extends AppCompatActivity {
+
+    public Button nazad;
+    public Button potvrdi;
+
+    public EditText imeListe;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_list);
+
+        nazad = findViewById(R.id.nazad);
+        potvrdi = findViewById(R.id.potvrdi);
+        imeListe = findViewById(R.id.imeListe);
+
+        DB = new DBHelper(this);
+
+        nazad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (AddList.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        potvrdi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ime = imeListe.getText().toString();
+
+                Boolean checkInsertData = DB.insertuserdata(ime);
+
+                if(checkInsertData) {
+                    Intent intent = new Intent(AddList.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
