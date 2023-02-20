5a607012161b9b2f0a7c0183fe8d7ca110639b79
Juninho99 <azunic1@etf.unsa.ba>
2023-02-20 15:44:49 +0100

    Dodana klasa AddList kao i njen xml, jednostavan prozor za dodavanje nove liste

---
+++ app/src/main/java/com/example/c3/AddList.java
@@ -0,0 +1,14 @@
+package com.example.c3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_list);
+    }
+}
