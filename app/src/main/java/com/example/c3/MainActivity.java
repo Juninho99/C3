47e928072fa6807193b960e5db0c14ee0cacface
Juninho99 <azunic1@etf.unsa.ba>
2023-02-20 15:40:27 +0100

    Dodana funkcija za prikaz listi u klasu MainActivity

--- app/src/main/java/com/example/c3/MainActivity.java
+++ app/src/main/java/com/example/c3/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.c3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button button1;
+    RecyclerView recyclerView;
+    ArrayList<String> editList;
+    DBHelper DB;
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        DB = new DBHelper(this);
+        editList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new MyAdapter(this, editList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        displaydata();
+    }
+
+    private void displaydata() {
+        Cursor cursor = DB.getdata();
+        if(cursor.getCount()==0)
+        {
+            Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else
+        {
+            while(cursor.moveToNext())
+            {
+                editList.add(cursor.getString(0));
+
+            }
+        }
     }
 }
